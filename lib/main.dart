b6280d9b4778484c8b6f878b7a695418f6cc2b7f
Krishna Adhikari <krishna.forkhaki@gmail.com>
2023-04-19 11:35:38 +0545

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@ import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
